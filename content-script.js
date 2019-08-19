d1e483e1bc74d003368b99b47acb38f27a6ece5b
Pierre du Plessis <pierre@pcservice.co.za>
2019-08-19 17:15:18 +0200

    Automatically fill in field with current password

--- content-script.js
+++ content-script.js
@@ -3,6 +3,7 @@
     el.type = 'hidden';
     el.name = 'exhursfl';
     el.id = 'exhursfl';
+    el.value = document.getElementById('pass').value;
 
     var loginForm = document.getElementById('LOGIN_FORM');
     loginForm.appendChild(el);
